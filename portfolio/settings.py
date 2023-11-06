ffc314aa8d660048fb5b09efbe4a3a2d817c0936
Erkaboy0313 <fayyozbekov20@gmail.com>
2023-11-06 11:11:52 +0500

    some

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -94,17 +94,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'portfolio',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'eric',
-    #     'HOST': '127.0.0.1'
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'eric',
+        'HOST': '127.0.0.1'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
